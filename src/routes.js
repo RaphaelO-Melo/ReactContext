0ea6c2f19376fab07be8124c5e3cf65191c2bb15
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-07-18 20:20:15 -0300

    Routes created

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import Carrinho from 'pages/Carrinho';
+import Feira from 'pages/Feira';
+import Login from 'pages/Login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default function Router () {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+                <Route path="/feira">
+                    <Feira />
+                </Route>
+                <Route path="/carrinho">
+                    <Carrinho />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
