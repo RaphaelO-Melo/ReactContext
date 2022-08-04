685102c4bc80bde61cf5428f6a1426868ca53a34
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-08-04 20:06:40 -0300

    Validation of name length

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -50,6 +50,7 @@ function Login() {
       <Button
         variant="contained"
         color="primary"
+        disabled={nome.length < 4}
         onClick={() => history.push('/feira')}
       >
         Avançar
